54f7fc185d79ec197601d5f29ca0378b68bd3fc2
Cora <silvative@gmail.com>
2024-07-03 18:21:44 +0100

    Update for new core map file format

--- csv-to-json.js
+++ csv-to-json.js
@@ -3,6 +3,14 @@ const parser = require("csv-parse/sync");
 const process = require("process");
 const yargs = require("yargs");
 
+function createTokenObject(token, ring) {
+  if ( ring.subject.scale ) { 
+    token.ring = ring
+    token.flags = {pf2e: {linkToActorSize: false}} 
+  } 
+  return token
+}
+
 const args = yargs(process.argv.slice(2))
       .command(
         "$0 <filename>",
@@ -10,8 +18,16 @@ const args = yargs(process.argv.slice(2))
         () => {
           yargs
             .positional("filename", {
-              describe: "A CSV filename. The first column of the CSV is ignored. Columns 2-6 should be compendium ID, actor ID, actor image path, token image path, and an optional scale-ratio antecedent (consequent of 1), and an optional boolean indicating whether random images are to be enabled.",
-            });
+              describe: `A CSV file. The columns should contain the following data:  
+              1- Label (ignored)
+              2- Compendium ID
+              3- Actor ID
+              4- Actor image path
+              5- Token image path
+              6- Optional scale-ratio antecedent (consequent of 1)
+              7- Optional boolean indicating whether random images are to be enabled"
+            `
+          });
         }
       )
       .usage("Usage: node $0 <filename>")
@@ -30,11 +46,23 @@ const jsonData = parser
         pack: row[1],
         id: row[2],
         actor: row[3],
-        token: { img: row[4], scale: Number(row[5]) || undefined, randomImg: !!row[6] || undefined }
+        token: {
+          randomImg: !!row[6] || undefined,
+          texture: {
+            src: row[4], 
+            scaleX: Number(row[5]) || undefined, 
+            scaleY: Number(row[5]) || undefined,   
+          }
+        },
+        ring: {
+          subject: {
+            scale: Number(row[5]) || undefined
+          }
+        }
     }))
     .reduce((accum, row) => {
         accum[row.pack] ??= {};
-        accum[row.pack][row.id] = { actor: row.actor, token: row.token };
+        accum[row.pack][row.id] = { actor: row.actor, token: createTokenObject(row.token, row.ring) };
         return accum;
     }, {});
 
