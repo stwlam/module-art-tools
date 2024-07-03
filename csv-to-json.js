7e7009f9a02c6bc27ffa27d0ec5ae26c526c5e65
Cora <silvative@gmail.com>
2024-07-03 21:38:03 +0100

    Update with new format

--- csv-to-json.js
+++ csv-to-json.js
@@ -10,7 +10,22 @@ const args = yargs(process.argv.slice(2))
         () => {
           yargs
             .positional("filename", {
-              describe: "A CSV filename. The data is used as follows: A = label, B = key, C = source. D = portrait, E = thumbnail, F = token, G = scale, H = subject. Tag groups are I through ??.",
+              describe: `Input is a CSV with the following data in each column: 
+              A = label (string)
+              B = key (string)
+              C = source book (string)
+              D = portrait (filepath)
+              E = thumbnail (filepath)
+              F = token (filepath)
+              G = subject (filepath)
+              H = scale (number)
+              I = ancestry tags (csv string => array)
+              J = armor tags (csv string => array)
+              K = equipment tags (csv string => array)
+              L = ability tags (csv string => array)
+              M = feature tags (csv string => array)
+              N = unique? (string)
+              `
             });
         }
       )
@@ -31,19 +46,20 @@ const data = parser
       "label": row[0],
       "key": row[1],
       "source": row[2],
+      "unique": row[13] || undefined,
       "art": {
         "portrait" : row[3],
         "thumb" : row[4],
         "token" : row[5],
-        "scale" : Number(row[6]) || undefined,
-        "subject" : row[7],
+        "subject" : row[6],
+        "scale" : Number(row[7]) || undefined,
       },
-      "tagGroups":  {
-        "family" : { "key": "family", "tags": row[8] ? row[8].toLowerCase().split(",") : undefined  },
-        "ancestry" : { "key": "ancestry", "tags": row[9] ? row[9].toLowerCase().split(",") : undefined  },
-        "equipment" : { "key": "equipment", "tags": row[10] ? row[10].toLowerCase().split(",") : undefined  },
-        "stance" : { "key": "stance", "tags": row[11] ? row[11].toLowerCase().split(",") : undefined  },
-        "armor" : { "key": "armor", "tags": row[12] ? row[12].toLowerCase().split(",") : undefined },
+      "tags":  {
+        "ancestry" : row[8] ? row[8].toLowerCase().split(",") : undefined,
+        "armor" : row[9] ? row[9].toLowerCase().split(",") : undefined,
+        "equipment" : row[10] ? row[10].toLowerCase().split(",") : undefined,
+        "abilities" : row[11] ? row[11].toLowerCase().split(",") : undefined,
+        "features" : row[12] ? row[12].toLowerCase().split(",") : undefined,
       },
     }))
     .map((element) => {
